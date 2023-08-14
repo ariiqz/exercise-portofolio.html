dc7c9f8da646464a7f128abce54ac0ab3a6299d5
ariiqz <afrahtama@student.telkomuniversity.ac.id>
2023-08-14 22:16:55 +0700

    #11 feature/blogs done

---
+++ script.js
@@ -0,0 +1,8 @@
+const students = ["student1", "student2", "student3"];
+let student2 = students [1];
+
+let student1 = students.find((item)=> {
+    return item ==="student1"
+})
+
+console.log(student1);
