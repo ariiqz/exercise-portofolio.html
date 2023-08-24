27695ae5209c380054292a5561cdee0404699ae9
ariiqz <afrahtama@student.telkomuniversity.ac.id>
2023-08-24 21:59:16 +0700

    person

--- assets/JS/person.js
+++ assets/JS/person.js
@@ -30,3 +30,31 @@ console.log(persons);
 
 const foundPerson = Person.find("Mr. Budi");
 console.log(foundPerson);
+
+// Latihan OOP
+
+class Student extends Person {
+  constructor(name, gender) {
+      super(name, gender);
+      this.class = ""; 
+      this.school = ""; 
+  }
+
+  setClass(className) {
+      this.class = className;
+      return this; 
+  }
+
+  setSchool(schoolName) {
+      this.school = schoolName;
+      return this;
+  }
+}
+
+const student = 
+new Student("sopo", "L")
+  .setClass("2th grade")
+  .setSchool("SMA");
+
+console.log(student);
+
